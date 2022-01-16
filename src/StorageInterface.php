0acf133e105e06e3c1fdb1a1ba5ae2899ca3a5fc
Radek Ziemniewicz <radek@quillstack.org>
2022-01-16 20:25:09 +0000

    Added method 'add'

--- src/StorageInterface.php
+++ src/StorageInterface.php
@@ -22,9 +22,14 @@ interface StorageInterface
     public function missing(string $path): bool;
 
     /**
-     * Saves the contents to the file.
+     * Saves the contents to the file, the existing file is overwritten.
      */
     public function save(string $path, mixed $contents): bool;
+
+    /**
+     * Saves the contents at the end of the file.
+     */
+    public function add(string $path, mixed $contents): bool;
 
     /**
      * Deletes one or more files.
